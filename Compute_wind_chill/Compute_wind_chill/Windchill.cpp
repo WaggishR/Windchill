e5c719888cff4ca807ba1de2d1e6611b625af194
John Klepeis <klepeisj@gmail.com>
2019-03-29 23:03:22 -0400

    wokring version

--- Compute_wind_chill/Compute_wind_chill/Windchill.cpp
+++ Compute_wind_chill/Compute_wind_chill/Windchill.cpp
@@ -1,11 +1,14 @@
+//John Klepeis
+//This program outputs wind chill factor when
+
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 using namespace std;
 
-void ctof(double& temp);
-void ftoc(double& temp);
-double windchill(double tempF, double wind);
+void ctof(double& tempctof);//converts temperature from c to f
+void ftoc(double& tempftoc);//converts temperature from f to c
+double windchill(double tempF, double wind);//finds winchill using the temperature in f and the wind speed
 
 int main() {
 	double temp; //temperature
@@ -24,30 +27,28 @@ int main() {
 	cout << "\nEnter the current temperature: ";
 	cin >> temp;
 
-	//convert to F for use in windchill
-	if (scale == 'c') 
+	
+	if (scale == 'c')//convert to F for use in windchill
 		ctof(temp);
-	else;
-
+		
 	WCF = windchill(temp, windspeed); //calculate windchill factor and assign to variable in main
 
-	if (scale == 'c')
-		ftoc(temp);
-	else;
+	if (scale == 'c')//convert back to c if needed
+		ftoc(WCF);
 
-	cout << fixed << setprecision(1) << "\nThe current wind chill factor is " << WCF << scale;
+	cout << fixed << setprecision(1) << "\nThe current wind chill factor is " << WCF << scale << endl;//output results in original scale with 1 decimal
 
 	system("pause");
 	return 0;
 
 }
 
-void ctof(double& temp) {//receives temp, and sends back new temp
-	temp = 1.8*temp+32;//formula to convert C to F
+void ctof(double& tempctof) {//receives temp, and sends back new temp
+	tempctof = 1.8*tempctof+32;//formula to convert C to F
 }
 
-void ftoc(double& temp) {//recieves temp and sends back new temp
-	temp = (temp - 32)*(5 / 9);//formula to convert F to C
+void ftoc(double& tempftoc) {//recieves temp and sends back new temp
+	tempftoc = (tempftoc - 32)/1.8;//formula to convert F to C
 }
 
 double windchill(double tempF, double wind) {//recieves temperature in F (tempF), wind speed (wind)
